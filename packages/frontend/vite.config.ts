9eb0e49c4bdd14a54e2ac07e97e07843e0701ac4
Marco Beier <marco-beier@t-online.de>
2023-01-04 00:58:46 +0100

    feat(proxy): proxying of requests to the backend during dev-time
    CORS~

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -5,11 +5,14 @@ export default defineConfig({
   plugins: [solidPlugin()],
   server: {
     port: 3000,
+    proxy: {
+      "^/todo/*": "http://localhost:4004", // CORS Auth Header not allowed CAP - https://vitejs.dev/config/server-options.html#server-proxy
+    },
   },
   build: {
     target: "esnext",
   },
   test: {
-    setupFiles: "./tests/setupVitest.ts"
-  }
+    setupFiles: "./tests/setupVitest.ts",
+  },
 });
