12246513304bdae37419dd3deb9d529fff7e38b4
Marco Beier <marco-beier@t-online.de>
2023-01-03 12:34:37 +0100

    refactor: simplify data store

--- packages/frontend/src/stores/todo.tsx
+++ packages/frontend/src/stores/todo.tsx
@@ -1,22 +1,17 @@
 import { createStore } from "solid-js/store";
 
 export type Todo = {
-  ID: string;
-  createdAt: string;
-  createdBy: string;
-  modifiedAt: string;
-  modifiedBy: string;
+  ID?: string;
+  createdAt?: string;
+  createdBy?: string;
+  modifiedAt?: string;
+  modifiedBy?: string;
   title: string;
   completed: boolean;
 };
 
 export const [todos, setTodos] = createStore<Todo[]>([
   {
-    ID: "",
-    createdAt: "",
-    createdBy: "",
-    modifiedAt: "",
-    modifiedBy: "",
     title: "",
     completed: false,
   },
@@ -29,5 +24,8 @@ export const actions = {
       "completed",
       (completed) => !completed
     );
+  },
+  addTodo(title: string) {
+    setTodos([...todos, { title, completed: false }]);
   },
 };
