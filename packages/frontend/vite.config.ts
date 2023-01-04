a88a479a305a12c973d71ee5c68ef9a43d489ea5
Marco Beier <marco-beier@t-online.de>
2023-01-04 01:48:14 +0100

    docs: add some documentation in vite config

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -6,7 +6,11 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "^/todo/*": "http://localhost:4004", // CORS Auth Header not allowed CAP - https://vitejs.dev/config/server-options.html#server-proxy
+      "^/todo/*": "http://localhost:4004",
+      // CORS Auth Header not allowed CAP - https://vitejs.dev/config/server-options.html#server-proxy
+      // alternatively during dev time: create own cap server impl that allows auth header by adding
+      // "access-control-allow-headers, authorization" to it's impl:
+      // https://cap.cloud.sap/docs/node.js/best-practices?q=cors#manual-implementation-1
     },
   },
   build: {
